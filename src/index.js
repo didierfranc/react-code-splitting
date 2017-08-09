e844c03a92695139e9214128e32b6ed49102ec93
Jorge Obregon <obregon08@gmail.com>
2017-08-09 13:47:21 -0400

    support for loader state

--- src/index.js
+++ src/index.js
@@ -1,32 +1,36 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 export default class Async extends React.Component {
   componentWillMount = () => {
-    this.cancelUpdate = false
-    this.props.load.then((c) => { 
-      this.C = c
-      if (!this.cancelUpdate) {
-        this.forceUpdate()
-      }
-    })
-  }
+    this.cancelUpdate = false;
+    this.props.load.then((c) => {
+      setTimeout(() => {
+        this.C = c;
+        if (!this.cancelUpdate) {
+          this.forceUpdate();
+        }
+      }, this.props.mockTimeout);
+    });
+  };
 
   componentWillUnmount = () => {
-    this.cancelUpdate = true
-  }
+    this.cancelUpdate = true;
+  };
 
   render = () => {
-    const {componentProps} = this.props
+    const { componentProps } = this.props;
     return this.C
       ? this.C.default
         ? <this.C.default {...componentProps} />
         : <this.C {...componentProps} />
-      : null
-    return this.C ? <this.C.default {...componentProps} /> : null
-  }
+      : (this.props.loader ? <this.props.loader/> : null);
+  };
 }
 
+// https://facebook.github.io/react/docs/typechecking-with-proptypes.html
 Async.propTypes = {
-  load: PropTypes.instanceOf(Promise).isRequired,
-}
+  load       : PropTypes.instanceOf(Promise).isRequired,
+  loader     : PropTypes.any,
+  mockTimeout: PropTypes.number,
+};
